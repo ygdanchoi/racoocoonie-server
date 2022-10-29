88692548d97c583552c0154f1c2dac928be6e70a
Dan Choi <ygdanchoi@gmail.com>
2022-10-29 13:35:28 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+let data = '[0,0,0]';
+
+app.get('/get', (req, res) => {
+  res.send(data);
+});
+
+app.get('/set', (req, res) => {
+  data = req.query.data
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
